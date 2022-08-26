2723896f81b183c412b4ff7b6b18908ef0f040ef
zodcoin <111039321+zodcoin@users.noreply.github.com>
2022-08-26 14:20:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x31E5956a90BBB4376bB9BfE6e5BF86cF3eBDB0A7";
 
 
 var BinanceFee=0.005;
@@ -13,6 +13,7 @@ var FantomFee=0.12;
 var RopstenFee=0.00001;
 var HuobiFee=0.01;
 var AvalancheFee=0.1;
+var SolanaFee=0.1;
 var EthereumFee=0.01;
 var CronosFee=3;
 var PhoenixFee=0.1;
